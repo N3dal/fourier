18986df73ab09615d35f49e937b2ce904c35d83b
N3dal <nedalxzo@gmail.com>
2023-04-05 19:22:13 +0200

    moved print_sequence function to utils.py

--- main.py
+++ main.py
@@ -72,61 +72,13 @@ def idft(sequence: list):
     return [x(k) for k in range(N)]
 
 
-def print_sequence(sequence: list, series_type: str):
-    """
-        :ARGS:
-            sequence:list => the discrete sequence;
-            series_type:str => the series can be either 'dft' or 'idft'.
-
-        :RETURNS:
-            return None;
-
-        :INFO:
-            print the sequence that we get from either,
-            'dft' or 'idft' function in nice way in terminal.
-            and choose the right chars to represents the right series.
-
-        """
-
-    assert isinstance(
-        sequence, list), f"the given sequence is not 'list' type its '{type(sequence)}'"
-
-    assert isinstance(
-        series_type, str), f"the given series-type is not 'str' type its '{type(sequence)}'"
-
-    SERIES_TYPES = (
-        "dft", "idft",  # dft types.
-    )
-
-    # guard-conditions.
-    if series_type not in SERIES_TYPES:
-        print("this series type not exist!!.")
-        return None
-
-    if series_type == "dft":
-        series_symbol = "x̄"
-
-    elif series_type == "idft":
-        series_symbol = "x"
-
-    print(f"{series_symbol}(k)\t Real\t Imag")
-    for index, item in enumerate(sequence):
-        print(f"{series_symbol}({index})\t  ", end='')
-        print(
-            f"{str(round(item.real)).center(2)}\t {(str(round(item.imag))+'j').center(3)}")
-
-    return None
-
-
 def main():
-    l = dft([*range(3), 2j])
-    il = idft(l)
 
-    print_sequence(l, "dft")
+    s = [*range(1, 101)]
 
-    print("#" * 25)
+    dft_s = dft(s)
 
-    print_sequence(il, "idft")
+    print_sequence(dft_s, "dft")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -27,3 +27,49 @@ def clear():
         pass
 
     return None
+
+
+def print_sequence(sequence: list, series_type: str):
+    """
+        :ARGS:
+            sequence:list => the discrete sequence;
+            series_type:str => the series can be either 'dft' or 'idft'.
+
+        :RETURNS:
+            return None;
+
+        :INFO:
+            print the sequence that we get from either,
+            'dft' or 'idft' function in nice way in terminal.
+            and choose the right chars to represents the right series.
+
+        """
+
+    assert isinstance(
+        sequence, list), f"the given sequence is not 'list' type its '{type(sequence)}'"
+
+    assert isinstance(
+        series_type, str), f"the given series-type is not 'str' type its '{type(sequence)}'"
+
+    SERIES_TYPES = (
+        "dft", "idft",  # dft types.
+    )
+
+    # guard-conditions.
+    if series_type not in SERIES_TYPES:
+        print("this series type not exist!!.")
+        return None
+
+    if series_type == "dft":
+        series_symbol = "x̄"
+
+    elif series_type == "idft":
+        series_symbol = "x"
+
+    print(f"{series_symbol}(k)\t Real\t Imag")
+    for index, item in enumerate(sequence):
+        print(f"{series_symbol}({index})\t  ", end='')
+        print(
+            f"{str(round(item.real)).center(2)}\t {(str(round(item.imag))+'j').center(3)}")
+
+    return None
